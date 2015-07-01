5f1e1c3941cc999c8bf47b2f416aa78f7da717cf
tomek <tomasz@strejczek.name>
2015-07-01 23:50:15 +0200

    Add basic Aurelia module

---
+++ src/evenucelusw/wwwroot/app.js
@@ -0,0 +1,13 @@
+import 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+
+export class App {
+  configureRouter(config, router){
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['','welcome'], name: 'welcome',  moduleId: './welcome',      nav: true, title:'Welcome' }
+    ]);
+
+    this.router = router;
+  }
+}
