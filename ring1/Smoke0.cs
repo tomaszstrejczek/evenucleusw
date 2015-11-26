fbe57b32b98cedf6fa1373d971196526c005858c
unknown <ts15187@cen4032134.ebre.pl>
2015-11-26 16:28:24 +0100

    Working on nancy tests

---
+++ ring1/Smoke0.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy;
+using Nancy.Testing;
+
+
+namespace ring1
+{
+    [TestClass]
+    public class Smoke0Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+			var bootstrapper = new TestingBootstrapper();
+			var browser = new Browser(bootstrapper);
+
+            var result = browser.Post("/api/login", with => {
+                with.HttpRequest();
+            });
+
+            // Then
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
---
+++ ring1/TestingBootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api;
+using Nancy;
+using Nancy.Testing;
+using Nancy.TinyIoc;
+
+namespace ring1
+{
+    public class TestingBootstrapper: ConfigurableBootstrapper
+    {
+        protected override IEnumerable<INancyModule> GetAllModules(TinyIoCContainer container)
+        {
+            return new Account[] {new Account()}.AsEnumerable();
+        }
+    }
+}
