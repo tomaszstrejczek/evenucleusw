502fa4a24696f36e429e15ae9c7febc5b04da687
Tomek Strejczek <tomasz@strejczek.name>
2015-12-31 18:33:26 +0100

    Added keys view

---
+++ webapp/keys/KeyCard.tsx
@@ -0,0 +1,36 @@
+﻿import * as React from 'react';
+
+import {owl} from './../utils/deepCopy';
+import {TsColor} from './../utils/colors';
+
+export interface KeyCardProperties {
+    key?: string;
+    keyDto: ts.dto.KeyInfoDto;
+    color: TsColor;
+}
+
+export class KeyCard extends React.Component<KeyCardProperties, any> {
+    render(): JSX.Element {
+        return (
+            <div className="col-md-4 col-xs-12 col-lg-3" style={{ margin: "5px" }}>
+                <div className="panel panel-default">
+                  <div className="panel-heading" style={{ background: this.props.color.darkest, color: "white" }} >
+                    <span>{this.props.keyDto.keyId}</span><span className="glyphicon glyphicon-trash pull-right"></span>
+                  </div>
+                  <div className="panel-body" style={{ padding: "0px"}}>
+                    <table className="table table-striped" style={{ marginBottom: "0px" }} >
+                      <tbody>
+                        {this.props.keyDto.pilots.map(pilot => {
+                                    return <tr key={"p_"+pilot.name}><td style={{width:"1px"}} ><img src={pilot.url} style={{ width: "24px", height: "24px" }}/></td><td style={{ verticalAlign: "middle" }} >{pilot.name}</td></tr>
+                        }) }
+                        {this.props.keyDto.corporations.map(corpo => {
+                            return <tr key={"c_" + corpo.name}><td style={{ width: "1px" }} ><img src={corpo.url} style={{ width: "24px", height: "24px" }}/></td><td style={{ verticalAlign: "middle" }} >{corpo.name}</td></tr>
+                        }) }
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+            </div>
+        );
+    }
+}
--- webapp/keys/keys.tsx
+++ webapp/keys/keys.tsx
@@ -1,10 +1,86 @@
 ﻿import * as React from 'react';
 
+import {yellow, blue} from './../utils/colors';
+import {KeyCard} from './KeyCard';
+
 export class Keys extends React.Component<any, any>{
     render(): JSX.Element {
+        var data = this.getTestData();
+
         return (
-            <p>Keys</p>
+            <div>
+                <div className="row">
+                {data.map(key => {
+                    return <KeyCard key={key.keyId.toString()} keyDto={key} color={yellow}/>;
+                }) }
+                    </div>
+                <div className="row">
+                    <div className="col-md-4 col-xs-12 col-lg-3" style={{ margin: "5px" }}>
+                        <div className="panel panel-default">
+                          <div className="panel-heading" style={{ background: blue.darkest, color: "white" }} >
+                            <span>Add new</span><span className="glyphicon glyphicon-plus pull-right"></span>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
+    }
+
+    getTestData(): Array<ts.dto.KeyInfoDto> {
+        var result = new Array<ts.dto.KeyInfoDto>();
+
+        var p1 = {
+            name: "stryju",
+            url: "https://image.eveonline.com/Character/1_64.jpg"
+        } as ts.dto.PilotDto;
+
+        var p2 = {
+            name: "pilot2",
+            url: "https://image.eveonline.com/Character/1_64.jpg"
+        } as ts.dto.PilotDto;
+
+        var p3 = {
+            name: "pilot3",
+            url: "https://image.eveonline.com/Character/1_64.jpg"
+        } as ts.dto.PilotDto;
+
+        var c1 = {
+            name: "corpo1",
+            url: "https://image.eveonline.com/Character/1_64.jpg"
+        } as ts.dto.CorporationDto;
+
+        var k: ts.dto.KeyInfoDto = {
+            keyInfoId: 1,
+            keyId: 12345678,
+            vCode: "alamakota",
+            userId: 1,
+            pilots: [p1],
+            corporations: []
+        };
+        result.push(k);
+
+        k = {
+            keyInfoId: 2,
+            keyId: 12345671,
+            vCode: "alamakota",
+            userId: 1,
+            pilots: [p1, p2, p3],
+            corporations: []
+        };
+        result.push(k);
+
+        k = {
+            keyInfoId: 3,
+            keyId: 12345672,
+            vCode: "alamakota",
+            userId: 1,
+            pilots: [],
+            corporations: [c1]
+        };
+        result.push(k);
+
+        return result;
     }
 };
 
